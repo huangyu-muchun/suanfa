2ca6c97e2220263a332a9c342e7eb9e6636ae333
huangyu666666 <1726823220@qq.com>
2020-10-29 22:33:19 +0800

    leetcode-4

---
+++ 3.无重复字符的最长子串.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=3 lang=javascript
+ *
+ * [3] 无重复字符的最长子串
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let arr = [], max = 0
+    for(let i = 0; i < s.length; i++) {
+        let index = arr.indexOf(s[i])
+        if(index !== -1) {
+            arr.splice(0, index+1);
+        }
+        arr.push(s.charAt(i))
+        max = Math.max(arr.length, max) 
+    }
+    return max
+};
+// @lc code=end
+
