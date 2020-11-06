3f9f508f518491c377b37cbd71c08c58c2df44b0
huangyu666666 <1726823220@qq.com>
2020-11-06 22:27:27 +0800

    leetcode-12

---
+++ 69.x-的平方根.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=69 lang=javascript
+ *
+ * [69] x 的平方根
+ */
+
+// @lc code=start
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function (x) {
+    let left = 1, right = x
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2)
+        if (mid * mid == x) {
+            return mid
+        }
+        else if (mid * mid < x) {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+    return right
+};
+// @lc code=end
+
